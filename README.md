45349f59e5d54819b55b8caa13a59f859ad6dee1
Tim Holy <tim.holy@gmail.com>
2015-06-01 12:58:09 -0500

    Initial working commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Ratios
 
 [![Build Status](https://travis-ci.org/timholy/Ratios.jl.svg?branch=master)](https://travis-ci.org/timholy/Ratios.jl)
+
+This package provides types similar to Julia's `Rational` type, which make some sacrifices but have better computational performance.
+
+Currently the only type provided is `SimpleRatio(num, den)` for two integers `num` and `den`.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,30 @@
 using Ratios
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+r = SimpleRatio(1,2)
+@test convert(Float64, r) == 0.5
+@test convert(Float32, r) == 0.5f0
+@test convert(BigFloat, r) == BigFloat(1)/2
+
+r2 = SimpleRatio(2,3)
+@test r*r2 == SimpleRatio(2,6) == SimpleRatio(1,3)
+@test r2*3 == 3*r2 == 2
+@test r*false == false*r == 0
+@test r/r2 == SimpleRatio(3,4)
+@test r/2 == SimpleRatio(1,4)
+@test 2/r == 4
+@test 4 == 2/r
+@test r+1 == 1+r == SimpleRatio(3,2)
+@test r-1 == SimpleRatio(-1,2)
+@test 1-r == r
+@test r+r2 == SimpleRatio(7,6)
+@test r-r2 == SimpleRatio(-1,6)
+@test r^2 == SimpleRatio(1,4)
+@test -r == SimpleRatio(-1,2)
+@test_approx_eq 0.2*r 0.1
+@test r == 0.5
+@test 0.5 == r
+
+@test_throws OverflowError -SimpleRatio(0x02,0x03)
+
+@test r + SimpleRatio(0x02,0x03) == SimpleRatio(7,6)
