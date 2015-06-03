015b3c28b3e5f093a7be69147377d33a88718d56
Simon Byrne <simonbyrne@gmail.com>
2015-06-03 10:46:42 +0100

    alias ratio to rational

--- src/Ratios.jl
+++ src/Ratios.jl
@@ -4,52 +4,5 @@ import Base: convert, promote_rule
 
 export SimpleRatio
 
-immutable SimpleRatio{T<:Integer} <: Real
-    num::T
-    den::T
-end
-
-convert{S}(::Type{BigFloat}, r::SimpleRatio{S}) = BigFloat(r.num)/r.den
-function convert{T<:FloatingPoint,S}(::Type{T}, r::SimpleRatio{S})
-    P = promote_type(T,S)
-    convert(T, convert(P, r.num)/convert(P, r.den))
-end
-convert{T<:Integer}(::Type{SimpleRatio{T}}, i::Integer) = SimpleRatio{T}(convert(T, i), one(T))
-
-*(x::SimpleRatio, y::SimpleRatio) = SimpleRatio(x.num*y.num, x.den*y.den)
-*(x::SimpleRatio, y::Bool) = SimpleRatio(x.num*y, x.den)
-*(x::SimpleRatio, y::Integer) = SimpleRatio(x.num*y, x.den)
-*(x::Bool, y::SimpleRatio) = SimpleRatio(x*y.num, y.den)
-*(x::Integer, y::SimpleRatio) = SimpleRatio(x*y.num, y.den)
-/(x::SimpleRatio, y::SimpleRatio) = SimpleRatio(x.num*y.den, x.den*y.num)
-/(x::SimpleRatio, y::Integer) = SimpleRatio(x.num, x.den*y)
-/(x::Integer, y::SimpleRatio) = SimpleRatio(x*y.den, y.num)
-+(x::Integer, y::SimpleRatio) = SimpleRatio(x*y.den + y.num, y.den)
--(x::Integer, y::SimpleRatio) = SimpleRatio(x*y.den - y.num, y.den)
-+(x::SimpleRatio, y::SimpleRatio) = SimpleRatio(x.num*y.den + x.den*y.num, x.den*y.den)
--(x::SimpleRatio, y::SimpleRatio) = SimpleRatio(x.num*y.den - x.den*y.num, x.den*y.den)
-^(x::SimpleRatio, y::Integer) = SimpleRatio(x.num^y, x.den^y)
-
--{T<:Signed}(x::SimpleRatio{T}) = SimpleRatio(-x.num, x.den)
--{T<:Unsigned}(x::SimpleRatio{T}) = throw(OverflowError())
-
-promote_rule{T<:Integer,S<:Integer}(::Type{SimpleRatio{T}}, ::Type{S}) = SimpleRatio{promote_type(T,S)}
-promote_rule{T<:Integer,S<:Integer}(::Type{SimpleRatio{T}}, ::Type{SimpleRatio{S}}) = SimpleRatio{promote_type(T,S)}
-promote_rule{T<:Integer,S<:FloatingPoint}(::Type{SimpleRatio{T}}, ::Type{S}) = promote_type(T,S)
-
-==(x::SimpleRatio, y::SimpleRatio) = x.num*y.den == x.den*y.num
-
-==(x::SimpleRatio, y::Integer) = x.num == x.den*y
-==(x::Integer, y::SimpleRatio) = x*y.den == y.num
-
-function ==(x::FloatingPoint, q::SimpleRatio)
-    if isfinite(x)
-        (count_ones(q.den) == 1) & (x*q.den == q.num)
-    else
-        x == q.num/q.den
-    end
-end
-
-==(q::SimpleRatio, x::FloatingPoint) = x == q
-
+typealias SimpleRatio{T} Rational{T}
 end
